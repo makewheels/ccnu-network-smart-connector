38f7d152214ae4aa7686177dbea414594f5aa59e
makewheels <makewheels>
2021-06-08 15:03:57 +0800

    雏形竣工

---
+++ src/main/java/com/eg/ccnu/net/NetTask.java
@@ -0,0 +1,73 @@
+package com.eg.ccnu.net;
+
+import cn.hutool.core.codec.Base64;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class NetTask {
+
+    private boolean isNetworkAvailable() {
+        HttpResponse response;
+        try {
+            response = HttpRequest.get("https://www.google.com/")
+                    .setConnectionTimeout(5000)
+                    .execute();
+        } catch (Exception e) {
+            return false;
+        }
+        int status = response.getStatus();
+        return status == HttpStatus.HTTP_OK;
+    }
+
+    /**
+     * 连接
+     *
+     * @return
+     */
+    private boolean connect() {
+        log.info("开始尝试重连");
+        HttpResponse response;
+        try {
+            response = HttpRequest.post("http://l.ccnu.edu.cn/0.htm")
+                    .body(Base64.decodeStr(
+                            "REREREQlM0QyMDIwMTgwMDExJTI1NDBjaGluYW5l" +
+                                    "dCUyNnVwYXNzJTNEY2NudTU2MTIxMjMlMjZzdWZ" +
+                                    "maXglM0QxJTI2ME1LS2V5JTNEMTIz"))
+                    .execute();
+        } catch (Exception e) {
+            log.warn("尝试重连抛异常: {}", e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+        if (response.getStatus() == 200) {
+            log.info("连接成功");
+            return true;
+        } else {
+            log.warn("连接失败，可能是请求有问题，http status = {}, response body = {}",
+                    response.getStatus(), response.body());
+            return false;
+        }
+    }
+
+    /**
+     * 断开
+     *
+     * @return
+     */
+    private boolean disConnect() {
+        //http://l.ccnu.edu.cn/F.htm
+        return true;
+    }
+
+    @Scheduled(fixedRate = 1000 * 3)
+    private void autoCheck() {
+        if (!isNetworkAvailable())
+            connect();
+    }
+}
