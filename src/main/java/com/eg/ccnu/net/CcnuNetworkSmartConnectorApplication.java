997a58757e9c98bb6173f63bbc2bf3366b522b9a
makewheels <makewheels>
2021-06-08 14:39:07 +0800

    Initial commit

---
+++ src/main/java/com/eg/ccnu/net/CcnuNetworkSmartConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.eg.ccnu.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcnuNetworkSmartConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcnuNetworkSmartConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eg/ccnu/net/CcnuNetworkSmartConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.ccnu.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcnuNetworkSmartConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
